134ce3370417a60792f780969816e5f5012a8832
jsjaramilloh <64642941+jsjaramilloh@users.noreply.github.com>
2021-03-13 16:57:27 -0500

    Add files via upload
    first commit

---
+++ do.js
@@ -0,0 +1,17 @@
+var data = {
+    'english': {
+        'title':'Welcome to HomeTechNow',
+        'description':
+            'The products you can find on your webpage range from Technology to Gadgets. For the client comfort, the purchases are online!. The delivery takes 1 to 2 days. The payment method is Cash on delivery, you can pay when you receive the product at your door. you have a full satisfaction guarantee. If you are not satisfied with the product, simply contact us and we will refund your money.'        
+    },
+    'spanish': {
+        'title':'Bienvenidos a HomeTechNow',
+        'description':
+            'Los productos que puede encontrar en su página web van desde Tecnología hasta Gadgets. Para la comodidad del cliente, ¡las compras son online !. El plazo de entrega es de 1 a 2 días. La forma de pago es Contra reembolso, puedes pagar cuando recibas el producto en tu puerta. tienes una garantía de satisfacción total. Si no está satisfecho con el producto, simplemente contáctenos y le reembolsaremos su dinero.'        
+    },
+    'italian': {
+        'title':'Benvenuto in HomeTechNow',
+        'description':
+            'The products I prodotti che puoi trovare sulla tua pagina web vanno dalla tecnologia ai gadget. Per la comodità del cliente, gli acquisti sono online !. La consegna richiede da 1 a 2 giorni. Il metodo di pagamento è il Contrassegno, puoi pagare quando ricevi il prodotto a casa tua. hai una garanzia di piena soddisfazione. Se non sei soddisfatto del prodotto, contattaci e ti rimborseremo.'       
+    },
+}
